d66b10b37d3f3fb8e8d96f5cb8ef3375370c202e
Josh Bowler <joshbowler46@gmail.com>
2020-01-10 02:49:21 -0700

    copleted

---
+++ Part1/index.js
@@ -0,0 +1,39 @@
+function play() {
+    const playerSpan = document.getElementById('player');
+    if (playerSpan.innerText === 'X') {
+        playerSpan.innerText = 'O';
+    } else {
+        playerSpan.innerText = 'X';
+    }
+}
+
+function play(clickedId) {
+    const playerSpan = document.getElementById('player');
+    const clickedElement = document.getElementById(clickedId);
+
+    if (playerSpan.innerText === 'X') {
+        playerSpan.innerText = 'O';
+        clickedElement.innerText = 'X';
+    } else {
+        playerSpan.innerText = 'X';
+        clickedElement.innerText = 'O';
+    }
+}
+
+const board = [];
+
+function play(clickedId) {
+    const playerSpan = document.getElementById('player');
+    const clickedElement = document.getElementById(clickedId);
+
+    if (playerSpan.innerText === 'X') {
+        playerSpan.innerText = 'O';
+        clickedElement.innerText = 'X';
+        board[clickedId] = 'X';
+    } else {
+        playerSpan.innerText = 'X';
+        clickedElement.innerText = 'O';
+        board[clickedId] = 'O';
+    }
+    console.log(board);
+}
